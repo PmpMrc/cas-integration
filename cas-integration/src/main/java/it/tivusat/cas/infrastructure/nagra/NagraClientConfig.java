db5d8241164130cd1551046c24a0c4ee1e142086
Marco Pompei <pompei.mrc@gmail.com>
2026-08-13 14:44:31 +0200

    Implementazione wiremock local per testing + modifica client config nagra

--- cas-integration/src/main/java/it/tivusat/cas/infrastructure/nagra/NagraClientConfig.java
+++ cas-integration/src/main/java/it/tivusat/cas/infrastructure/nagra/NagraClientConfig.java
@@ -3,7 +3,10 @@ package it.tivusat.cas.infrastructure.nagra;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 @EnableConfigurationProperties(NagraProperties.class)
@@ -11,8 +14,13 @@ public class NagraClientConfig {
 
     @Bean
     public RestClient nagraRestClient(NagraProperties properties) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(5));
+        requestFactory.setReadTimeout(Duration.ofSeconds(15));
+
         return RestClient.builder()
                 .baseUrl(properties.baseUrl())
+                .requestFactory(requestFactory)
                 .build();
     }
 }
